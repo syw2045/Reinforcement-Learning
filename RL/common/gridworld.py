1c19c23a8c87b4477d26188b9a6401838b23fadc
syw2045 <syw2045@naver.com>
2025-01-13 15:12:15 +0900

    add gridworld and render

---
+++ RL/common/gridworld.py
@@ -0,0 +1,69 @@
+import numpy as np
+import common.gridworld_render as render_helper
+
+class GridWorld:
+    def __init__(self):
+        self.action_space = [0, 1, 2, 3] # 행동 공간(가능한 행동들)
+        self.action_meaning = {
+            0: 'UP',
+            1: 'DOWN',
+            2: 'LEFT',
+            3: 'RIGHT',
+        }
+        
+        self.reward_map = np.array(
+            [[0, 0, 0, 1.0],
+             [0, None, 0, -1.0],
+             [0, 0, 0, 0]]
+        )
+
+        self.goal_state = (0,3) # 목표 State 좌표
+        self.wall_state = (1,1) # 벽 State 좌표
+        self.state_state = (2,0) # 시작 State 좌표
+        self.agent_state = self.state_state # 에이전트 초기 State 좌표
+
+    @property
+    def height(self):
+        return len(self.reward_map)
+    
+    @property
+    def width(self):
+        return len(self.reward_map[0])
+    
+    @property
+    def shape(self):
+        return self.reward_map.shape
+    
+    def actions(self):
+        return self.action_space # [0, 1, 2, 3]
+    
+    def states(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                yield (h,w)
+
+    def next_state(self, state, action):
+        action_move_map = [(-1,0), (1,0), (0, -1), (0,1)] # 상하좌우
+        move = action_move_map[action]
+        next_state = (state[0] + move[0], state[1] + move[1]) # X, Y 값 계산
+        ny, nx = next_state
+
+        if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height:
+            next_state = state # 테두리 밖
+        elif next_state == self.wall_state:
+            next_state = state # 벽
+
+        return next_state
+    
+    def reward(self, state, action, next_state):
+        return self.reward_map[next_state]
+    
+    def render_v(self, v=None, policy=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                            self.wall_state)
+        renderer.render_v(v, policy, print_value)
+
+    def render_q(self, q=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                          self.wall_state)
+        renderer.render_q(q, print_value)
