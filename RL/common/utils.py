f292f5246d4f10bfc332795dbdb84c778eb7d760
syw2045 <syw2045@naver.com>
2025-01-19 19:43:40 +0900

    sarsa, Q-learning

---
+++ RL/common/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def argmax(xs):
+    idxes = [i for i, x in enumerate(xs) if x == max(xs)]
+    if len(idxes) == 1:
+        return idxes[0]
+    elif len(idxes) == 0:
+        return np.random.choice(len(xs))
+
+    selected = np.random.choice(idxes)
+    return selected
+
+def greedy_probs(Q, state, epsilon=0, action_size=4):
+    qs = [Q[(state, action)] for action in range(action_size)]
+    max_action = argmax(qs)  # OR np.argmax(qs)
+    base_prob = epsilon / action_size
+    action_probs = {action: base_prob for action in range(action_size)}  #{0: ε/4, 1: ε/4, 2: ε/4, 3: ε/4}
+    action_probs[max_action] += (1 - epsilon)
+    return action_probs
