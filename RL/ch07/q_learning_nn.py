ea3c327ff760e3ea746a4524c312a35409ede0f3
syw2045 <syw2045@naver.com>
2025-01-21 14:45:10 +0900

    Q-Learning

---
+++ RL/ch07/q_learning_nn.py
@@ -0,0 +1,105 @@
+import os, sys; sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import matplotlib.pyplot as plt
+import numpy as np
+from dezero import Model
+from dezero import optimizers
+import dezero.functions as F
+import dezero.layers as L
+from common.gridworld import GridWorld
+
+def one_hot(state):
+    HEIGHT, WIDTH = 3,4
+    vec = np.zeros(HEIGHT * WIDTH, dtype=np.float32)
+    y, x = state
+    idx = WIDTH * y + x
+    vec[idx] = 1.0
+    return vec[np.newaxis, :]
+
+class QNet(Model):
+    def __init__(self):
+        super().__init__()
+        self.l1 = L.Linear(100) # 중간층의 크기
+        self.l2 = L.Linear(4) # 행동의 크기
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = self.l2(x)
+        return x
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma =0.9
+        self.lr = 0.01
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        self.qnet = QNet() #신경망 초기화
+        self.optimizer = optimizers.SGD(self.lr) # 옵티마이저 생성
+        self.optimizer.setup(self.qnet) # 옵티마이저에 신경망 등록
+
+    def get_action(self, state_vec):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            qs = self.qnet(state_vec)
+            return qs.data.argmax()
+
+    def update(self, state, action, reward, next_state, done):
+        # 다음 상태에서 최대가 되는 Q 함수의 값(next_q) 계산
+        if done:
+            next_q = np.zeros(1) # [0.]
+        else:
+            next_qs = self.qnet(next_state)
+            next_q = next_qs.max(axis=1)
+            next_q.unchain() # next_q를 역전파에서 제외
+
+        target = self.gamma * next_q + reward
+        qs = self.qnet(state)
+        q = qs[:,action]
+        loss = F.mean_squared_error(target,q)
+
+        self.qnet.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+
+        return loss.data
+
+
+env = GridWorld()
+agent = QLearningAgent()
+
+episodes = 1000
+loss_history = []
+
+for episode in range(episodes):
+    state = env.reset()
+    state = one_hot(state)
+    total_loss, cnt =0,0
+    done = False
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done = env.step(action)
+        next_state = one_hot(next_state)
+
+        loss = agent.update(state, action, reward, next_state, done)
+        total_loss += loss
+        cnt += 1
+        state = next_state
+
+    average_loss = total_loss / cnt
+    loss_history.append(average_loss)
+
+# [그림 7-14] 에피소드별 손실 추이
+plt.xlabel('episode')
+plt.ylabel('loss')
+plt.plot(range(len(loss_history)), loss_history)
+plt.show()
+
+# [그림 7-15] 신경망을 이용한 Q 러닝으로 얻은 Q 함수와 정책
+Q = {}
+for state in env.states():
+    for action in env.action_space:
+        q = agent.qnet(one_hot(state))[:, action]
+        Q[state, action] = float(q.data)
+env.render_q(Q)
