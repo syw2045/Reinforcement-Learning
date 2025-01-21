049403f5ddf26fc453842373a334aaa1c0074cd5
syw2045 <syw2045@naver.com>
2025-01-21 15:47:54 +0900

    DQN cartpole

---
+++ RL/ch08/dqn.py
@@ -0,0 +1,130 @@
+import copy
+from dezero import Model
+from dezero import optimizers
+import dezero.functions as F
+import dezero.layers as L
+import random
+from collections import deque
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
+    
+    def __len__(self):
+        return len(self.buffer)
+    
+    def get_batch(self):
+        data = random.sample(self.buffer, self.batch_size)
+
+        state = np.stack([x[0] for x in data])
+        action = np.stack([x[1] for x in data])
+        reward = np.stack([x[2] for x in data])
+        next_state = np.stack([x[3] for x in data])
+        done = np.stack([x[4] for x in data]).astype(np.int32)
+
+        return state,action,reward,next_state,done
+    
+class QNet(Model):
+    def __init__(self, action_size):
+        super().__init__()
+        self.l1 = L.Linear(128)
+        self.l2 = L.Linear(128)
+        self.l3 = L.Linear(action_size)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
+        return x
+    
+class DQNAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.lr = 0.0005
+        self.epsilon = 0.1
+        self.buffer_size = 10000
+        self.batch_size = 32
+        self.action_size = 2
+
+        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.qnet = QNet(self.action_size)
+        self.qnet_target = QNet(self.action_size)
+        self.optimizer = optimizers.Adam(self.lr)
+        self.optimizer.setup(self.qnet)
+
+    def sync_qnet(self):
+        self.qnet_target = copy.deepcopy(self.qnet)
+
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            state=state[np.newaxis, :]
+            qs = self.qnet(state)
+            return qs.data.argmax()
+        
+    def update(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        if len(self.replay_buffer) < self.batch_size:
+            return
+        
+        state, action, reward, next_state, done = self.replay_buffer.get_batch()
+
+        qs = self.qnet(state)
+        q = qs[np.arange(self.batch_size), action]
+
+        next_qs = self.qnet_target(next_state)
+        next_q = next_qs.max(axis=1)
+        next_q.unchain()
+        target = reward + (1-done) * self.gamma * next_q
+
+        loss = F.mean_squared_error(q,target)
+
+        self.qnet.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+
+
+episodes = 300      # 에피소드 수
+sync_interval = 20  # 신경망 동기화 주기(20번째 에피소드마다 동기화)
+env = gym.make('CartPole-v0', render_mode='rgb_array')
+agent = DQNAgent()
+reward_history = [] # 에피소드별 보상 기록
+
+for episode in range(episodes):
+    state = env.reset()[0]
+    done = False
+    total_reward = 0
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        done = terminated | truncated
+
+        agent.update(state, action, reward, next_state, done)
+        state = next_state
+        total_reward += reward
+
+    if episode % sync_interval == 0:
+        agent.sync_qnet()
+
+    reward_history.append(total_reward)
+    if episode % 10 == 0:
+        print("episode :{}, total reward : {}".format(episode, total_reward))
+
+
+# [그림 8-8] 「카트 폴」에서 에피소드별 보상 총합의 추이
+plt.xlabel('Episode')
+plt.ylabel('Total Reward')
+plt.plot(range(len(reward_history)), reward_history)
+plt.show()
+
+
