9ae9ce001178c19f493e7a6460f920c7a491754f
syw2045 <syw2045@naver.com>
2025-02-12 16:23:39 +0900

    DDQN

--- RL/Algorithms/DDQN.py
+++ RL/Algorithms/DDQN.py
@@ -1,169 +1,166 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from collections import namedtuple
+from collections import deque
 import random
+import numpy as np
 import gym
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
-from torch import optim
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+# from IPython.display import display
+
+# def display_frames_as_gif(frames):
+#     pil_frames = [Image.fromarray(frame) for frame in frames]
+#     pil_frames[0].save('cartpole_animation.gif', save_all=True, append_images=pil_frames[1:], duration=50, loop=0)
+
+HIDDEN_SIZE = 128
+NUM_EPISODES = 100
+SYNC_INTERVAL = 20
 
 
+class ReplayBuffer:
+    def __init__(self, buffer_size, batch_size):
+        self.buffer = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
 
-Transition = namedtuple(
-    'Transition', ('state', 'action', 'next_state', 'reward'))
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done)
+        self.buffer.append(data)
 
-GAMMA = 0.99
-MAX_STEPS = 200
-NUM_EPISODES = 500
-
-class ReplayMemory:
-    def __init__(self, CAPACITY):
-        self.capacity = CAPACITY
-        self.memory = []
-        self.index = 0
-
-    def push(self, state, action, state_next, reward):
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        
-        self.memory[self.index] = Transition(state, action, state_next, reward)
-        self.index = (self.index + 1) % self.capacity
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-    
     def __len__(self):
-        return len(self.memory)
-    
-class Net(nn.Module):
-    def __init__(self, n_in, n_mid, n_out):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(n_in, n_mid)
-        self.fc2 = nn.Linear(n_mid, n_mid)
-        self.fc3 = nn.Linear(n_mid, n_out)
+        return len(self.buffer)
 
-    def forward(self, x):
-        h1 = F.relu(self.fc1(x))
-        h2 = F.relu(self.fc2(h1))
-        output = self.fc3(h2)
-        return output
+    def get_batch(self):
+        data = random.sample(self.buffer, self.batch_size)
 
-BATCH_SIZE = 32
-CAPACITY = 10000
+        state = torch.tensor(np.stack([x[0] for x in data]))
+        action = torch.tensor(np.array([x[1] for x in data]).astype(np.compat.long))
+        reward = torch.tensor(np.array([x[2] for x in data]).astype(np.float32))
+        next_state = torch.tensor(np.stack([x[3] for x in data]))
+        done = torch.tensor(np.array([x[4] for x in data]).astype(np.int32))
+        return state, action, reward, next_state, done
 
-class Brain:
-    def __init__(self, num_states, num_actions):
-        self.num_actions = num_actions
-        self.memory = ReplayMemory(CAPACITY)
 
-        n_in, n_mid, n_out = num_states, 32, num_actions
-        self.main_q_network = Net(n_in, n_mid, n_out)
-        self.target_q_network = Net(n_in, n_mid, n_out)
-        print(self.main_q_network)
+class QNet(nn.Module):
+    def __init__(self, state_size, action_size):
+        super().__init__()
+        self.l1 = nn.Linear(state_size, HIDDEN_SIZE)
+        self.l2 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
+        self.value = nn.Linear(HIDDEN_SIZE, action_size)
 
-        self.optimizer = optim.Adam(
-            self.main_q_network.parameters(), lr=0.0001)
+    def forward(self, state):
+        l1 = F.relu(self.l1(state))
+        l2 = F.relu(self.l2(l1))
+        value = self.value(l2)
+        return value
+
+
+class DDQNAgent:
+    def __init__(self):
+        self.gamma = 0.98
+        self.lr = 0.0005
         
-    def replay(self):
-        if len(self.memory) < BATCH_SIZE:
-            return
+        self.epsilon_decay = 0.999
+        self.epsilon = 1.0
+        self.epsilon_end = 0.1
         
-        self.batch, self.state_batch, self.action_batch, self.reward_batch, self.non_final_next_states = self.make_minibatch()
-        self.expected_q_function = self.get_expected_q_function()
-        self.update_main_q_network()
+        self.buffer_size = 10000
+        self.batch_size = 32
+        self.state_size = 4
+        self.action_size = 2
 
-    def decide_action(self, state, episode):
-        epsilon = 0.5 * (1 / (episode + 1))
-        if epsilon <= np.random.uniform(0, 1):
-            self.main_q_network.eval()
-            with torch.no_grad():
-                action = self.main_q_network(state).max(1)[1].view(1, 1)
+        self.replay_buffer = ReplayBuffer(self.buffer_size, self.batch_size)
+        self.qnet = QNet(self.state_size, self.action_size)
+        self.qnet_target = QNet(self.state_size, self.action_size)
+        self.optimizer = optim.Adam(self.qnet.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        if self.epsilon > self.epsilon_end:
+            self.epsilon *= self.epsilon_decay
+        
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
         else:
-            action = torch.LongTensor(
-                [[random.randrange(self.num_actions)]])
-        return action
-    
-    def make_minibatch(self):
-        transitions = self.memory.sample(BATCH_SIZE)
-        batch = Transition(*zip(*transitions))
+            state = torch.tensor(state[np.newaxis, :])
+            qs = self.qnet(state)
+            return qs.argmax().item()
 
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    def update(self, state, action, reward, next_state, done):
+        self.replay_buffer.add(state, action, reward, next_state, done)
+        if len(self.replay_buffer) < self.batch_size:
+            return
+
+        state, action, reward, next_state, done = self.replay_buffer.get_batch()
+        qs = self.qnet(state)
+        q = qs[np.arange(len(action)), action]
+
+        ### DDQN ###
+        next_qs = self.qnet(next_state)
+        next_actions = next_qs.argmax(1)
+
+        next_qs_target = self.qnet_target(next_state)
+        next_q = next_qs_target[np.arange(len(action)), next_actions]
         
-        return batch, state_batch, action_batch, reward_batch, non_final_next_states
-    
-    def get_expected_q_function(self):
-        self.main_q_network.eval()
-        self.target_q_network.eval()
+        #######
 
-        self.state_action_values = self.main_q_network(self.state_batch).gather(1, self.action_batch)
-        
-        non_final_mask = torch.BoolTensor(tuple(map(lambda s: s is not None, self.batch.next_state)))
-        next_state_values = torch.zeros(BATCH_SIZE)
-        action_max = torch.zeros(BATCH_SIZE).type(torch.LongTensor)
+        target = reward + (1 - done) * self.gamma * next_q
 
-        action_max[non_final_mask] = self.main_q_network(self.non_final_next_states).detach().max(1)[1]
-        action_max_final_next_states = action_max[non_final_mask].view(-1, 1)
+        loss_fn = nn.MSELoss()
+        loss = loss_fn(q, target)
 
-        next_state_values[non_final_mask] = self.target_q_network(self.non_final_next_states).gather(1, action_max_final_next_states).detach().squeeze()
-        expected_q_function = self.reward_batch + GAMMA * next_state_values.detach()
-        return expected_q_function
-    
-    def update_main_q_network(self):
-        self.main_q_network.train()
-        loss = F.smooth_l1_loss(self.state_action_values, self.expected_q_function.unsqueeze(1).requires_grad_())
-        
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-    def update_target_q_network(self):
-        self.target_q_network.load_state_dict(self.main_q_network.state_dict())
+    def sync_qnet(self):
+        self.qnet_target.load_state_dict(self.qnet.state_dict())
 
-class Agent:
-    def __init__(self, num_states, num_actions):
-        self.brain = Brain(num_states, num_actions)
 
-    def update_q_function(self):
-        self.brain.replay()
-
-    def get_action(self, state, episode):
-        return self.brain.decide_action(state, episode)
-    
-    def memorize(self, state, action, state_next, reward):
-        self.brain.memory.push(state, action, state_next, reward)
-
-    def update_target_q_function(self):
-        self.brain.update_target_q_network()
-
-class Environment:
+class DDQNEnv:
     def __init__(self):
         self.env = gym.make("CartPole-v1", render_mode="human")
+        #self.env = gym.make('CartPole-v1', render_mode='rgb_array')
+
         self.env.reset()
-        num_states = self.env.observation_space.shape[0]
-        num_actions = self.env.action_space.n
-        self.agent = Agent(num_states, num_actions)
+        self.reward_history = []
+
+        self.gif_frames = []
 
     def run(self):
         for episode in range(NUM_EPISODES):
-            observation, _ = self.env.reset()
-            state = torch.from_numpy(observation).float().unsqueeze(0)
-            for step in range(MAX_STEPS):
-                action = self.agent.get_action(state, episode)
-                observation_next, _, done, _, _ = self.env.step(action.item())
-                state_next = None if done else torch.from_numpy(observation_next).float().unsqueeze(0)
-                self.agent.memorize(state, action, state_next, torch.FloatTensor([1.0 if not done else -1.0]))
-                self.agent.update_q_function()
-                state = state_next
-                if done:
-                    break
-            if episode % 2 == 0:
-                self.agent.update_target_q_function()
+            state = self.env.reset()[0]
+            done = False
+            total_reward = 0
+
+            while not done:
+                action = agent.get_action(state)
+                next_state, reward, terminated, truncated, info = self.env.step(action)
+                done = terminated | truncated
+
+                # To make gif
+                # if episode % 10 == 0:
+                #     frame = self.env.render()
+                #     self.gif_frames.append(frame)
+
+                agent.update(state, action, reward, next_state, done)
+                state = next_state
+                total_reward += reward
+                
+            if episode % SYNC_INTERVAL == 0:
+                agent.sync_qnet()
+
+            self.reward_history.append(total_reward)
+            if episode % 10 == 0:
+                print("episode :{}, total reward : {}".format(episode, total_reward))
+
+        # display_frames_as_gif(self.gif_frames)
+        self.env.close()
+        
 
 if __name__ == "__main__":
-    cartpole_env = Environment()
-    cartpole_env.run()
+    env = DDQNEnv()
+    agent = DDQNAgent()
+    env.run()
