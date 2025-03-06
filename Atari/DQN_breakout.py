3dcebe9d0ba43bcc562b6190cddb39a4cf60d482
syw2045 <syw2045@naver.com>
2025-03-06 16:27:01 +0900

    DQN & BreakOut-v5

--- Atari/DQN_breakout.py
+++ Atari/DQN_breakout.py
@@ -14,32 +14,32 @@ from torch.utils.tensorboard import SummaryWriter
 state_size = [4, 84, 84]  # (C, H, W)
 action_size = 4  # Noop / Fire / Right / Left
 
-load_model = False
-train_mode = True
+load_model = True
+train_mode = False
 
 batch_size = 32
-mem_maxlen = 50000
+mem_maxlen = 10000
 discount_factor = 0.99
 learning_rate = 0.00025
 
 run_step = 50000 if train_mode else 0
 test_step = 5000
 train_start_step = 5000
-target_update_step = 500
+target_update_step = 500 # update 주기
 
 print_interval = 10
 save_interval = 100
 
 epsilon_eval = 0.05
 epsilon_init = 1.0 if train_mode else epsilon_eval
-epsilon_min = 0.1
+epsilon_min = 0.2
 explore_step = run_step * 0.8
 eplsilon_delta = (epsilon_init - epsilon_min)/explore_step if train_mode else 0
 
 # Model save and load path
 date_time = datetime.datetime.now().strftime("%y%m%d%H%M%S")
 save_path = f"./saved_models/Atari/DQN/{date_time}"
-load_path = "./saved_models/Atari/DQN/"
+load_path = "./saved_models/Atari/DQN/250306150226"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -48,26 +48,20 @@ class DQN(torch.nn.Module):
     def __init__(self, **kwargs):
         super(DQN, self).__init__(**kwargs)
         self.conv1 = torch.nn.Conv2d(in_channels=state_size[0], out_channels=32, kernel_size=8, stride=4)
-        dim1 = ((state_size[1] - 8)//4 + 1, (state_size[2] - 8)//4 + 1)
-        
         self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        dim2 = ((dim1[0] - 4)//2 + 1, (dim1[1] - 4)//2 + 1)
-        
         self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
-        dim3 = ((dim2[0] - 3)//1 + 1, (dim2[1] - 3)//1 + 1)
-
-        # self.flat = torch.nn.Flatten() # FC의 입력을 위해 1차원으로 변경
-        self.fc1 = torch.nn.Linear(64*dim3[0]*dim3[1], 512)
+        self.flat = torch.nn.Flatten()
+        self.fc1 = torch.nn.Linear(7*7*64, 512)
         self.q = torch.nn.Linear(512, action_size)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.fc1(x.view(x.size(0), -1)))
-        return self.q(x)
-
-
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc1(x)
+        x = self.q(x)
+        return x
 
 # DQN_Agent
 class DQNAgent:
@@ -88,18 +82,20 @@ class DQNAgent:
         
     # Epsilon greedy
     def get_action(self, state, training=True):
-        # print("stacked_state shape:", state.shape)
         self.network.train(training)
         epsilon = self.epsilon if training else epsilon_eval
 
         # 랜덤하게 action 선택
         if epsilon > random.random():  
             action = np.random.randint(0, action_size)
+            return action
+        # 아니라면 Q function을 가장 크게하는 action 선택
         else:
-            q = self.network(torch.FloatTensor(state).to(device))
-            action = torch.argmax(q, axis=-1, keepdim=True).data.cpu().numpy()
-        
-        return action
+            q = self.network(torch.FloatTensor(state).unsqueeze(0).to(device))
+            action = torch.argmax(q, axis=-1).data.cpu().numpy()
+            return action[0]
+    
+
 
     def append_sample(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -112,29 +108,24 @@ class DQNAgent:
         next_state = np.stack([b[3] for b in batch], axis=0)
         done       = np.stack([b[4] for b in batch], axis=0)
 
-        state      = torch.FloatTensor(state).to(device)  # (32, 4, 84, 84)
-        next_state = torch.FloatTensor(next_state).to(device)  # (32, 4, 84, 84)
-        
-        action = torch.LongTensor(action).unsqueeze(1).to(device)  # (32,) → (32, 1)
-        reward = torch.FloatTensor(reward).unsqueeze(1).to(device)  # (32,) → (32, 1)
-        done = torch.FloatTensor(done).unsqueeze(1).to(device)  # (32,) → (32, 1)
+        state, action, reward, next_state, done = map(lambda x: torch.FloatTensor(x).to(device),
+                                                            [state, action, reward, next_state, done])
 
-        # Q-value 계산
-        q_values = self.network(state)  # (32, 4) (각 액션에 대한 Q값)
-        q = q_values.gather(1, action)  # 선택된 action의 Q값만 가져옴 → (32, 1)
+        eye = torch.eye(action_size).to(device)
+        one_hot_action = eye[action.view(-1).long()]
+        q = (self.network(state) * one_hot_action).sum(1, keepdims=True)
+
         with torch.no_grad():
-            next_q_values = self.target_network(next_state)  # (32, 4)
-            max_next_q = next_q_values.max(1, keepdim=True)[0]  # (32, 1)
-            target_q = reward + (1 - done) * discount_factor * max_next_q  # (32, 1)
+            next_q = self.target_network(next_state)
+            target_q = reward + next_q.max(1, keepdims=True).values * ((1 - done) * discount_factor)
 
-        # 손실 계산
         loss = F.smooth_l1_loss(q, target_q)
 
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+        self.optimizer.zero_grad() # gradient 초기화 
+        loss.backward() # backprop을 통해 gradient 계산
+        self.optimizer.step() # model parameters update
 
-        # Epsilon decay
+        # epsilon decay
         self.epsilon = max(epsilon_min, self.epsilon - eplsilon_delta)
 
         return loss.item()
@@ -162,7 +153,7 @@ def preprocessing(state):
     return normalized_state.astype(np.float32)
 
 if __name__ == '__main__':
-    env = gym.make("ALE/Breakout-v5", render_mode="rgb_array")
+    env = gym.make("ALE/Breakout-v5", render_mode="human")
     state, _ = env.reset() # state : (210, 160, 3)
     frames = deque(maxlen=4)
     # Agent
@@ -180,17 +171,16 @@ if __name__ == '__main__':
             train_mode = False
 
         stacked_state = np.stack(frames, axis=0)
-       # print("stacked_state",stacked_state.shape)
-        action = agent.get_action(stacked_state) # 에이전트의 행동 선택
+        action = agent.get_action(stacked_state)
         next_state, reward, done, _, _ = env.step(action)
 
         frames.append(preprocessing(next_state))
-        state = preprocessing(next_state) # state 업데이트
+        state = preprocessing(next_state)
         next_stacked_state = np.stack(frames, axis=0)
-       # print("next_stacked_state" ,next_stacked_state.shape)
         score += reward
+
         if train_mode:
-            agent.append_sample(stacked_state, action, reward, next_stacked_state, done)
+            agent.append_sample(stacked_state, [action], [reward], next_stacked_state, [done])
 
         if train_mode and step > max(batch_size, train_start_step):
             # Training
